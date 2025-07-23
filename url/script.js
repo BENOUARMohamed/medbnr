6909c74018889bb48620f86fed1f94ace83359f2
Mohammed BENOUAR <moh@medbnr.com>
2025-07-23 14:21:17 +0100

    Add logging for HomeMenu click event and toggle active class

--- url/script.js
+++ url/script.js
@@ -23,6 +23,7 @@ document.querySelectorAll(".brain-svg circle").forEach(circle => {
 const HomeMenu = document.querySelector('.home-menu');
 HomeMenu.addEventListener('click', (event) => {
       console.log(event);
+      console.log(HomeMenu.classList);
       HomeMenu.classList.toggle("active");
     });
 
